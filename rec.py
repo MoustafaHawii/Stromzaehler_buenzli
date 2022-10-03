56cc65c451b9e744ab8afa7e9becd8c7f735df51
AndrasTarlos <tarlosa@bzz.ch>
2022-10-03 16:27:39 +0200

    Finalized upload_files() in rec.py. Now you can upload files to the server.

--- rec.py
+++ rec.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, request, flash, redirect
 import os
-import werkzeug
+from werkzeug.utils import secure_filename
 
 rec = Blueprint("rec", __name__, static_folder="static", template_folder="templates")
 path = os.getcwd()
+
 
 UPLOAD_FOLDER = os.path.join(path, 'uploads')
 
@@ -11,7 +12,7 @@ UPLOAD_FOLDER = os.path.join(path, 'uploads')
 @rec.route("/rec_xml_file", methods = ['POST', 'GET'])
 def rec_files():
     if request.method == 'POST':
-        if 'file' not in request.files:
+        if 'file-input' not in request.files:
             flash('No file part')
             return "No file sent"
             #return redirect(request.url)
@@ -20,8 +21,9 @@ def rec_files():
             return '.' in filename and filename.rsplit('.', 1)[1].lower() in "xml"
 
         # Save files into a list
-        uploaded_files = request.files.getlist("file")
-        #print(len(uploaded_files))
+        uploaded_files = request.files.getlist("file-input")
+        for f in uploaded_files:
+            print(f.filename)
         
         # Check if directory exists and if not, then create one 
         if not os.path.isdir(UPLOAD_FOLDER):
@@ -30,7 +32,7 @@ def rec_files():
         # Save files if the file has .xml type
         for file in uploaded_files:
             if file and allowed_file(file.filename):
-                file.save(os.path.join(rec.config("UPLOAD_FOLDER")), werkzeug.secure_filename(file.filename))
+                file.save(os.path.join(UPLOAD_FOLDER, secure_filename(file.filename)))
 
         flash('File(s) successfully uploaded')
         return redirect("/")
