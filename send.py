c93daaaf9e4ac2b1b271a61af27b34d4bca3ddbc
AndrasTarlos <tarlosa@bzz.ch>
2022-10-03 14:51:31 +0200

    Added features to backend

--- send.py
+++ send.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, jsonify
 
 send = Blueprint("send", __name__, static_folder="static", template_folder="templates")
 
-@send.route("/send")
-def home():
-    return render_template("index.html")
+# Sends every deduplicated data to the client in a json file 
+@send.route('/send_json_data', methods = ['POST', 'GET'])
+def send_json_data():
+    return jsonify({
+        "sensorId": "ID742",
+        "data": {
+        "ts": "2022-09-02T07:31:06Z",
+        "feedR": "1.2",
+        "usgR": "3",
+        "feedA": "2999",
+        "usgA": "2000"
+    }})
