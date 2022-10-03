4388a8321d5628b7003a874b037c5f04afa16e3b
AndrasTarlos <tarlosa@bzz.ch>
2022-10-03 16:03:04 +0200

    rec.py changes

--- rec.py
+++ rec.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, flash
+from flask import Blueprint, request, flash, redirect
 import os
 import werkzeug
 
@@ -11,12 +11,17 @@ UPLOAD_FOLDER = os.path.join(path, 'uploads')
 @rec.route("/rec_xml_file", methods = ['POST', 'GET'])
 def rec_files():
     if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return "No file sent"
+            #return redirect(request.url)
         # Function for checking if the filename contains xml 
         def allowed_file(filename):
             return '.' in filename and filename.rsplit('.', 1)[1].lower() in "xml"
 
         # Save files into a list
-        uploaded_files = request.files.getlist("file[]")
+        uploaded_files = request.files.getlist("file")
+        #print(len(uploaded_files))
         
         # Check if directory exists and if not, then create one 
         if not os.path.isdir(UPLOAD_FOLDER):
@@ -28,5 +33,5 @@ def rec_files():
                 file.save(os.path.join(rec.config("UPLOAD_FOLDER")), werkzeug.secure_filename(file.filename))
 
         flash('File(s) successfully uploaded')
-        return 'File(s) uploaded successfully'
+        return redirect("/")
     return "No file selected or false method was used"
