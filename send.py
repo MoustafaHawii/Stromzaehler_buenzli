26cf6fb8d2d2c19ca3016529338f215b8bb071d4
CuddlyBunion341 <daniel@bengl.cz>
2022-10-04 11:14:32 +0200

    added more dummy data

--- send.py
+++ send.py
@@ -5,10 +5,19 @@ send = Blueprint("send", __name__, static_folder="static", template_folder="temp
 # Sends every deduplicated data to the client in a json file 
 @send.route('/send_json_data', methods = ['POST', 'GET'])
 def send_json_data():
-    return jsonify([{
+    return jsonify([
+        {
             "ts": "2022-09-02T07:31:06Z",
             "feedR": 1.2,
             "usgR": 3,
             "feedA": 2999,
             "usgA": 2000
-        }])
+        },
+        {
+            "ts": "2022-10-02T07:31:06Z",
+            "feedR": 1.4,
+            "usgR": 3,
+            "feedA": 2999,
+            "usgA": 2000
+        },
+    ])
