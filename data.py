87efa678b2a6d581287ea493d25389fc0c75861c
CuddlyBunion341 <daniel@bengl.cz>
2022-10-05 08:48:49 +0200

    extracted relative values from sdat

--- data.py
+++ data.py
@@ -4,21 +4,25 @@ class Data:
         self.usgR = usgR
         self.feedA = feedA
         self.usgA = usgA
-        
+
     # Getter feedR
     def get_feedR(self):
         return self.feedR
+
     # Getter usgR
     def get_usgR(self):
         return self.usgR
+
     # Getter feedA
     def get_feedA(self):
         return self.feedA
+
     # Getter usgA
     def get_usgA(self):
         return self.usgA
-    
-        
-    
-        
-    
+
+    # To string
+    def __str__(self):
+        return "Data(feedR={},usgR={},feedA={},usgA={})".format(
+            self.feedR, self.usgR, self.feedA, self.usgA
+        )
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import os
 import xml.dom.minidom
 import dateutil.parser as dp
+from dateutil.relativedelta import *
+from data import Data
+import json
+
+# Get element value using selector
+def get_element_value(element, selector):
+    return element.getElementsByTagName(selector)[0].firstChild.nodeValue
 
 
 # Calculate the needed absolute values
@@ -16,18 +23,19 @@ def get_searched_row_value(row_values):
             sum_feedA += float(r.getAttribute("value"))
         if r.getAttribute("obis") == "1-1:2.8.2":
             sum_feedA += float(r.getAttribute("value"))
-            
+
     return [sum_feedA, sum_usgA]
+
 
 def get_highest_absolute_value_from_esl_files():
     sum_feedA = 0
     sum_usgA = 0
     highest_time = 0.0
     # Path to the ESL-Files
-    path_of_the_directory= os.getcwd() + "/static/files/ESL-Files"
+    path_of_the_directory = os.getcwd() + "/static/files/ESL-Files"
     # Iterate through the files in this directory
     for filename in os.listdir(path_of_the_directory):
-        f = os.path.join(path_of_the_directory,filename)
+        f = os.path.join(path_of_the_directory, filename)
         if os.path.isfile(f):
             # Open file
             with open(f, "r") as file:
@@ -42,36 +50,50 @@ def get_highest_absolute_value_from_esl_files():
                             sum_feedA = values[0]
                             sum_usgA = values[1]
                             highest_time = dp.parse(t.getAttribute("end")).timestamp()
-    
+
     return [sum_usgA, sum_feedA, highest_time]
 
 
-def parse_sdat(path = os.getcwd() + "/static/files/SDAT-Files"):
-    cnt = 0
+def parse_sdat(path=os.getcwd() + "/static/files/SDAT-Files"):
+    dict = {}
     for file in os.listdir(path):
         fullname = os.path.join(path, file)
         dom = xml.dom.minidom.parse(fullname)
         # ID of the Meter
-        id = dom.getElementsByTagName('rsm:DocumentID')[0].firstChild.nodeValue[-3:]
+        id = get_element_value(dom, "rsm:DocumentID")[-3:]
         # Start Datetime of the Observation-List
-        start_datetime = dom.getElementsByTagName("rsm:StartDateTime")[1].firstChild.nodeValue
+        start_datetime = get_element_value(dom, "rsm:StartDateTime")
+
+        date = dp.parse(start_datetime)
 
         # Über alle Einträge in der Liste der Observations iterieren und einen Eintrag erstellen.
         observation_list = dom.getElementsByTagName("rsm:Observation")
         for observation in observation_list:
-            value = observation.getElementsByTagName("rsm:Volume")[0].firstChild.nodeValue
-            cnt+=1
-            print(value)
-        print(id)
-        print(start_datetime)
-    print(cnt)
+            value = get_element_value(observation, "rsm:Volume")
+            value = round(float(value), 3)
+            # print(id, date, value)
+            date += relativedelta(minutes=+15)
+            key = int(date.timestamp() * 1000)
+            if key not in dict:
+                dict[key] = {}
+            if id == "742":
+                dict[key]["usgR"] = value
+            else:
+                dict[key]["feedR"] = value
+    return dict
+
+
+def dict_to_json(data):
+    arr = []
+    for key in sorted(data.keys()):
+        value = data[key]
+        arr.append({"ts": key, **value})
+    return arr
 
 
 if __name__ == "__main__":
     print("Getting 1 ESL value....")
     usgA, feedA, ts = get_highest_absolute_value_from_esl_files()
     print("Found!")
-    output = {ts: [None, None, feedA, usgA]}
-    parse_sdat()
-    
-    print(output)
+    data = parse_sdat()
+    print(json.dumps(dict_to_json(data), indent=4))
